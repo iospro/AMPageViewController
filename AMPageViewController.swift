8b1255f72738cf32d36aef08444da4a735674055
Alexey Matveev <2malex@gmail.com>
2022-06-05 15:41:59 +0300

    Revert "new features"
    This reverts commit 054765f5960da6c60525484c02cbe044977ef343.

--- AMPageViewController.swift
+++ AMPageViewController.swift
@@ -29,25 +29,11 @@
 import UIKit
 
 /// UIPageViewController subclass with very simple interface.
-/// It allows use cycled navigation without any well-known exceptions and handle UITableView cell swipes.
-class AMPageViewController: UIPageViewController {
-    
-    private struct Const {
-        /// when allowsTableCellSwipes = true it sets boundary insets to observe cell swipes
-        static let swipeThreshold = 40.0
-    }
+/// It allows use cycled navigation without any well-known exceptions.
+@objc class AMPageViewController: UIPageViewController {
 
     /// Turn on/off PageControl at the bottom
     @objc var showPageControl: Bool = true
-    
-    /// Enable cycle scroll
-    @objc var looping: Bool = true
-        
-    /// Boundary bounces on swipe without looping
-    @objc var bounceEnabled: Bool = true
-    
-    /// Permission to handle UITableView cell swipes
-    @objc var allowsTableCellSwipes: Bool = false
     
     /// Array of all viewControllers
     @objc var source: [UIViewController]? {
@@ -61,8 +47,6 @@ class AMPageViewController: UIPageViewController {
                 dataSource = nil
                 delegate = nil
             }
-            
-            updateBounceStrategy()
         }
     }
     
@@ -71,7 +55,7 @@ class AMPageViewController: UIPageViewController {
         
         get {
             var currentPageIndex: Int = 0
-            if let vc = viewControllers?.first, let source = source, let pageIndex = source.firstIndex(of: vc) {
+            if let vc = viewControllers?.first, let source = source, let pageIndex = source.index(of: vc) {
                 currentPageIndex = pageIndex
             }
             
@@ -82,23 +66,17 @@ class AMPageViewController: UIPageViewController {
             guard newValue >= 0, let source = source, newValue < source.count else { return }
             
             let vc = source[newValue]
-            let direction: UIPageViewController.NavigationDirection = newValue < pageIndex ? .reverse : .forward
-            if viewControllers?.first != vc {
-                setViewController(vc, direction: direction, animated: animationEnabled)
-            }
+            let direction: UIPageViewControllerNavigationDirection = newValue < pageIndex ? .reverse : .forward
             
-            if !animationEnabled {
-                animationEnabled = true // after first page set without animation
-            }
+            setViewController(vc, direction: direction)
         }
     }
     
     
-    /// override delegate set for empty source
     override weak var delegate: UIPageViewControllerDelegate? {
         
         get {
-            super.delegate
+            return super.delegate
         }
             
         set {
@@ -110,16 +88,11 @@ class AMPageViewController: UIPageViewController {
             }
         }
     }
-
-    private var animationEnabled: Bool = false
-    
-    private var queuingScrollView: UIScrollView?
-    
     
     /// Initializer in scroll-mode with interPageSpacing
-    @objc init(navigationOrientation: UIPageViewController.NavigationOrientation = .horizontal, interPageSpacing: Int = 5) {
+    @objc init(navigationOrientation: UIPageViewControllerNavigationOrientation = .horizontal, interPageSpacing: Int = 0) {
         
-        let options = (interPageSpacing > 0) ? [UIPageViewController.OptionsKey.interPageSpacing : interPageSpacing] : nil
+        let options = (interPageSpacing > 0) ? [UIPageViewControllerOptionInterPageSpacingKey : 5] : nil
         
         super.init(transitionStyle: .scroll, navigationOrientation: navigationOrientation, options: options)
     }
@@ -128,74 +101,31 @@ class AMPageViewController: UIPageViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    /// Set viewcontroller by index from source
+    @objc func setPageIndex(_ index: Int, completion: ((Bool) -> Void)? = nil) {
         
-        updateBounceStrategy()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        if !showPageControl {
-            view.subviews.forEach { sv in
-                if let scrollView = sv as? UIScrollView {
-                    scrollView.frame.size.height = view.frame.height
-                }
-            }
-        }
-    }
-    
-    /// Set ViewController by index from source
-    @objc func setPageIndex(_ index: Int, animated: Bool, completion: ((Bool) -> Void)? = nil) {
-        
-        guard index >= 0, let source = source, index < source.count else { return }
+        guard index > 0, let source = source, index < source.count else { return }
         
         let vc = source[index]
-        let direction: UIPageViewController.NavigationDirection = index < pageIndex ? .reverse : .forward
+        let direction: UIPageViewControllerNavigationDirection = index < pageIndex ? .reverse : .forward
         
-        setViewController(vc, direction: direction, animated: animated, completion: completion)
+        setViewController(vc, direction: direction, completion: completion)
     }
     
 
-    private func setViewController(_ viewController: UIViewController, direction: UIPageViewController.NavigationDirection = .forward, animated: Bool, completion: ((Bool) -> Void)? = nil) {
+    private func setViewController(_ viewController: UIViewController, direction: UIPageViewControllerNavigationDirection = .forward, completion: ((Bool) -> Void)? = nil) {
         
-        super.setViewControllers([viewController], direction: direction, animated: animated, completion: completion)
-    }
-    
-    private func updateBounceStrategy() {
-        
-        let shouldBounce = looping && source?.count ?? 0 > 2
-        let forbidBounces = !shouldBounce && !bounceEnabled
-
-        view.subviews.forEach { sv in
-            if let scrollView = sv as? UIScrollView {
-                                
-                scrollView.delegate = forbidBounces ? self : nil
-                
-                if allowsTableCellSwipes {
-                    queuingScrollView = scrollView
-                    
-                    let pan = UIPanGestureRecognizer(target: self, action: nil)
-                    scrollView.addGestureRecognizer(pan)
-                    pan.delegate = self
-                }
-            }
-        }
+        super.setViewControllers([viewController], direction: direction, animated: false, completion: completion)
     }
 }
 
-extension AMPageViewController: UIPageViewControllerDataSource {
+extension FFPageViewController: UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
-        guard let source = source, let index = source.firstIndex(of: viewController) else { return nil }
+        guard let source = source, let index = source.index(of: viewController) else { return nil }
         
         let count = source.count
-        
-        if !looping, (index - 1) < 0 {
-            return nil
-        }
         
         if count == 2, index == 0 {
             return nil
@@ -203,7 +133,7 @@ extension AMPageViewController: UIPageViewControllerDataSource {
         
         let prevIndex = (index - 1) < 0 ? count - 1 : index - 1
         
-        let pageContentViewController = source[prevIndex]
+        let pageContentViewController: UIViewController = source[prevIndex]
         
         return pageContentViewController
     }
@@ -211,13 +141,9 @@ extension AMPageViewController: UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         
-        guard let source = source, let index = source.firstIndex(of: viewController) else { return nil }
+        guard let source = source, let index = source.index(of: viewController) else { return nil }
         
         let count = source.count
-        
-        if !looping, (index + 1) >= count {
-            return nil
-        }
         
         if count == 2, index == 1 {
             return nil
@@ -229,71 +155,18 @@ extension AMPageViewController: UIPageViewControllerDataSource {
         
         return pageContentViewController
     }
-}
-
-extension AMPageViewController {
+    
     
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         
-        showPageControl ? (source?.count ?? 0) : 0
+        return showPageControl ? (source?.count ?? 0) : 0
     }
+    
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         
-        showPageControl ? pageIndex : 0
-    }
-}
-
-extension AMPageViewController: UIScrollViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-                
-        let currentPage = pageIndex
-        let pageCount = source?.count ?? 0
+        guard showPageControl else { return 0 }
         
-        if currentPage == 0, scrollView.contentOffset.x + scrollView.contentInset.left < 0 {
-            scrollView.contentOffset = CGPoint(x: -scrollView.contentInset.left, y: scrollView.contentOffset.y)
-        }
-        else if currentPage == pageCount - 1, scrollView.contentInset.right < 0, scrollView.contentOffset.x + scrollView.contentInset.right > 0 {
-            scrollView.contentOffset = CGPoint(x: -scrollView.contentInset.right, y: scrollView.contentOffset.y)
-        }
-    }
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-
-        let currentPage = pageIndex
-        let pageCount = source?.count ?? 0
-        
-        if currentPage == 0, scrollView.contentOffset.x + scrollView.contentInset.left < 0 {
-            targetContentOffset.pointee = CGPoint(x: -scrollView.contentInset.left, y: targetContentOffset.pointee.y)
-        }
-        else if currentPage == pageCount - 1, scrollView.contentInset.right < 0, scrollView.contentOffset.x + scrollView.contentInset.right > 0 {
-            targetContentOffset.pointee = CGPoint(x: -scrollView.contentInset.right, y: targetContentOffset.pointee.y)
-        }
-    }
-}
-
-extension AMPageViewController: UIGestureRecognizerDelegate {
-    
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        
-        guard let queuingScrollView = queuingScrollView, gestureRecognizer.state == .began else { return true }
-        
-        let isQueuingScrollView = otherGestureRecognizer.view == queuingScrollView
-                
-        if allowsTableCellSwipes {
-            
-            let isTableView = otherGestureRecognizer.view is UITableView
-            
-            let location = gestureRecognizer.location(in: view)
-
-            let allowedArea = min(location.x, view.bounds.width - location.x) > Const.swipeThreshold
-            let allow = (isQueuingScrollView && allowedArea) || (isTableView && !allowedArea)
-                        
-            return allow
-        }
-        else {
-            return isQueuingScrollView
-        }
+        return pageIndex
     }
 }
